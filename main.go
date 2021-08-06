2453087b3b39a73dfdb8355f16428b3a8db4692b
ckyrouac <ckyrouac@redhat.com>
2021-08-06 09:51:11 -0400

    stash

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	kafka "github.com/RedHatInsights/strimzi-client-go/apis/kafka.strimzi.io/v1beta1"
+	kafka "github.com/RedHatInsights/strimzi-client-go/apis/kafka.strimzi.io/v1beta2"
 	"io/ioutil"
 )
 
